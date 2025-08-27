f591c83e6dfd007d9bc4a5edbe9b6df9116f0ea0
heyywillz <atabisawilliams@gmail.com>
2025-08-27 17:14:05 +0000

    building my portfolio website

--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,3 @@
-// const navToggle = document.querySelector(".nav-toggle");
-// const nav = document.querySelector("nav");
-
-// navToggle.addEventListener("click", () => {
-//   nav.classList.toggle("active");
-
-//   navToggle.innerHTML = nav.classList.contains("active")
-//     ? '<i class="fa fa-times"></i>'
-//     : '<i class="fa fa-bars"></i>';
-// });
-
 const navToggle = document.querySelector(".nav-toggle");
 const nav = document.querySelector("nav");
 
